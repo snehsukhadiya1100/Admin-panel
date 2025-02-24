404f8a6e174f2affef797271cbba5b1a25bd7e92
sneh sukhadiya <141477255+snehsukhadiya1100@users.noreply.github.com>
2025-02-24 20:59:17 +0530

    Add files via upload

---
+++ themepart/sidebar.php
@@ -0,0 +1,131 @@
+<!-- <aside> -->
+    <div id="sidebar" class="nav-collapse">
+        <!-- sidebar menu start-->
+        <div class="leftside-navigation">
+            <ul class="sidebar-menu" id="nav-accordion">
+                <li>
+                    <a href="index.html">
+                        <i class="fa fa-dashboard"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                
+                <!-- <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-book"></i>
+                        <span>UI Elements</span>
+                    </a>
+                    <ul class="sub">
+						<li><a href="typography.html">Typography</a></li>
+						<li><a href="glyphicon.html">glyphicon</a></li>
+                        <li><a href="grids.html">Grids</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="fontawesome.html">
+                        <i class="fa fa-bullhorn"></i>
+                        <span>Font awesome </span>
+                    </a>
+                </li> -->
+                <li class="sub-menu">
+                    <a class="active" href="jav">
+                        <i class="fa fa-tasks"></i>
+                        <span>Product</span>
+                    </a>
+                    <ul class="sub">
+                        <li><a class="active" href="add-product.php">Add</a></li>
+                    </ul>
+                    <ul class="sub">
+                        <li><a class="active" href="display-product.php">View</a></li>
+                    </ul>
+
+                    <li class="sub-menu">
+                    <a class="active" href="jav">
+                        <i class="fa fa-tasks"></i>
+                        <span>category</span>
+                    </a>
+                    <ul class="sub">
+                        <li><a class="active" href="category.php">Add</a></li>
+                    </ul>
+                    <ul class="sub">
+                        <li><a class="active" href="display-category.php">View</a></li>
+                    </ul>
+                </li>
+
+                <li class="sub-menu">
+                    <a class="active" href="jav">
+                        <i class="fa fa-tasks"></i>
+                        <span>Addmin</span>
+                    </a>
+                    <ul class="sub">
+                        <li><a class="active" href="admin.php">Add</a></li>
+                    </ul>
+                    <ul class="sub">
+                        <li><a class="active" href="display-category.php">View</a></li>
+                    </ul>
+                </li>
+
+                <li class="sub-menu">
+                    <a class="active" href="jav">
+                        <i class="fa fa-tasks"></i>
+                        <span>User</span>
+                    </a>
+                    <ul class="sub">
+                        <li><a class="active" href="user.php">Add</a></li>
+                    </ul>
+                    <ul class="sub">
+                        <li><a class="active" href="display-user.php">View</a></li>
+                    </ul>
+                </li>
+
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-envelope"></i>
+                        <span>Mail </span>
+                    </a>
+                    <ul class="sub">
+                        <li><a href="mail.html">Inbox</a></li>
+                        <li><a href="mail_compose.html">Compose Mail</a></li>
+                    </ul>
+                </li>
+                <!-- <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class=" fa fa-bar-chart-o"></i>
+                        <span>Charts</span>
+                    </a>
+                    <ul class="sub">
+                        <li><a href="chartjs.html">Chart js</a></li>
+                        <li><a href="flot_chart.html">Flot Charts</a></li>
+                    </ul>
+                </li>
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class=" fa fa-bar-chart-o"></i>
+                        <span>Maps</span>
+                    </a>
+                    <ul class="sub">
+                        <li><a href="google_map.html">Google Map</a></li>
+                        <li><a href="vector_map.html">Vector Map</a></li>
+                    </ul>
+                </li> -->
+                <!-- <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-glass"></i>
+                        <span>Extra</span>
+                    </a>
+                    <ul class="sub">
+                        <li><a href="gallery.html">Gallery</a></li>
+						<li><a href="404.html">404 Error</a></li>
+                        <li><a href="registration.html">Registration</a></li>
+                    </ul>
+                </li> -->
+                <li>
+                    <a href="login.php">
+                        <i class="fa fa-user"></i>
+                        <span>Login Page</span>
+                    </a>
+                </li>
+            </ul>            </div>
+        <!-- sidebar menu end-->
+    </div>
+<!-- </aside> -->
